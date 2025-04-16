419524e9d0721bfd4d31a30a062b6beb7c297529
butcha89 <80286237+butcha89@users.noreply.github.com>
2025-04-16 23:02:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,28 @@ setup(
         'matplotlib',
         'seaborn',
         'dash',
-        'plotly'
+        'plotly',
+        'scikit-learn',
+        'configparser'
     ],
+    entry_points={
+        'console_scripts': [
+            'stash-tools=main:main',
+        ],
+    },
+    author='Your Name',
+    description='Tools for analyzing and managing Stash data',
+    long_description=open('README.md').read() if open('README.md').read() else '',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/stash-api-tools',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.7',
+    default_command='stats'
 )
