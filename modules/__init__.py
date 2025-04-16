0d9f5e627b59e66147cc487d5a9b82f63a8dff04
butcha89 <80286237+butcha89@users.noreply.github.com>
2025-04-16 23:16:32 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,5 @@
-
+# Initialisierung der Module
+# Importiere nur die Klassen, die direkt benötigt werden
+from .statistics import StatisticsModule
+from .recommendations import RecommendationModule
+from .discord import DiscordModule
